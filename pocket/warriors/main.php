5d4a853fbc06ce13808fa600ec06a51bc47ba8a6
Wantedkillers <Wantedkillerss@users.noreply.github.com>
2017-12-07 14:57:18 -0500

    Update main.php

--- pocket/warriors/main.php
+++ pocket/warriors/main.php
@@ -11,4 +11,40 @@ use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\utils\TextFormat;
 class Tag extends PluginBase{
-     public function onEnable(){
+     
+     public function GetBlock(){
+
+          $this->getServer()->getFile("block.yml") 
+               and if($file= true)
+                  $this->getServer()->getBlock($iD)
+                    
+                    public function returnBlock(){
+                    
+                    {
+                         
+                              $this->getServer()->getPlayer()->getBlock()->getBlockStatus()
+                                   
+                                   case:
+                         null
+                                        {
+                              
+                                        if(status = null())
+                                             
+                                             break;
+                              
+                              case:
+                        broken
+                                        {
+                              
+                                        if(status = brokenBlock())
+                      {
+                         $this->getServer()->getBlock(($iD)
+                              $this->getServer()->returnBlock($iD)
+                                   
+                                   and(if($id) = true())
+                            $this->getServer()->getFile()->("healthstatus.yml")      
+                               $this->getServer()->getFile()->("healthstatus.yml"); ("healthamount")  ; 
+                              $this->getServer()->getPlayer()->setHealth("X") //dependant on health amount 
+                    
+                    
+                  
