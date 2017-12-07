038073416786a5029d46c20ac9b5c0d2ba6d3475
Wantedkillers <Wantedkillerss@users.noreply.github.com>
2017-12-07 14:37:56 -0500

    Update main.php

--- pocket/warriors/main.php
+++ pocket/warriors/main.php
@@ -1,3 +1,14 @@
 <?php
 
 
+<?php
+namespace Wantedkillerss\HideTag;
+use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\Plugin;
+use pocketmine\Player;
+use pocketmine\command\CommandExecutor;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat;
+class Tag extends PluginBase{
+     public function onEnable(){
